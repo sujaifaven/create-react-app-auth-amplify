73677ec45c5f8906cdcfdff473e0388af336db2b
sujaifaven <78295833+sujaifaven@users.noreply.github.com>
2021-02-11 09:50:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React.
           </a>
         </header>
       </div>
